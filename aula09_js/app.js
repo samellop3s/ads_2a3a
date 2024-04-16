3644592997f93023690ee0cff622ffe6da9250b4
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2024-04-16 08:51:24 -0300

    att

--- aula09_js/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/',(req,res)=>{
-    res.send('OLA MUNDO DOS DEVS LOKOS DA MENTE !!!');
-});
-const app = express();
-app.use('/', router);
-module.exports = app; 
--- aula09_js/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-app.set('port',7777);
-const server = app.listen(app.get('port'),()=>{
-    console.log("Servidor rodando na porta: " + server.address().port);
-});
